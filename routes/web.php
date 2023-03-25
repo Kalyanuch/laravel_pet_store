8f7f9d1ce04e5cf0262e99e9d525a35a01973f79
Shamrylo Mykola <itlutsk@gmail.com>
2023-03-25 21:11:49 +0200

    Add base authentication.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +16,16 @@ use App\Http\Controllers\Admin\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('front.homepage');
-
-// Admin routes group
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function() {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
-    Route::resource('/categories', CategoryController::class);
-    Route::resource('/products', ProductController::class);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
